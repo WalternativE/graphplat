0166b1a4cb6716c3ead71a00683dc9c1e643cb3b
WalternativE <gregor.beyerle@gmail.com>
2018-04-02 23:52:48 +0200

    initial commit

---
+++ src/Client/Graph.fs
@@ -0,0 +1,68 @@
+module Graph
+
+open Fable.Core
+open Fable.Core.JsInterop
+open Elmish
+open Cytoscape
+open System
+open Fable.Import.React
+
+module R = Fable.Helpers.React
+module C = Cytoscape
+
+type Model =
+    { Id : string
+      Core : C.Cytoscape.Core option }
+
+type Msg =
+    | ContainerAvailable of Fable.Import.Browser.HTMLElement
+    | ContainerUnavailable
+
+let init () : Model * Cmd<Msg> =
+    let model =
+        let guid = Guid.NewGuid()
+        { Id = guid.ToString(); Core = None }
+    model, Cmd.none
+
+module B = Fable.Import.Browser
+
+let update (msg : Msg) (model : Model) : Model * Cmd<Msg> =
+    match msg with
+    | ContainerAvailable el ->
+        let opts = createEmpty<C.Cytoscape.CytoscapeOptions>
+        opts.container <- (Some el)
+        let core = C.cytoscape.cytoscape opts
+
+        // just for dev
+        B.console.log <| sprintf "%A" core
+
+        { model with Core = Some core }, Cmd.none
+    | ContainerUnavailable ->
+        { model with Core = None }, Cmd.none
+
+let refHandler (element : Fable.Import.Browser.Element) =
+    if isNull element then
+        ContainerUnavailable
+    else
+        ContainerUnavailable
+
+module IR = Fable.Import.React
+
+type [<Pojo>] GraphProps =
+    { Id : string
+      Dispatch : Msg -> unit }
+
+type GraphComponent(initialProps) =
+    inherit IR.Component<GraphProps, obj>(initialProps)
+
+    override this.componentDidMount() =
+        let el = B.document.getElementById this.props.Id
+        ContainerAvailable el |> this.props.Dispatch
+
+    override this.render () =
+        R.div [ R.Props.Id this.props.Id ] []
+
+let inline graph props children = R.ofType<GraphComponent, _, _> props children
+
+let view (model : Model) (dispatch : Msg -> unit) =
+    graph { Id = model.Id; Dispatch = dispatch } []
---
+++ src/Server/Server.fs
@@ -0,0 +1,43 @@
+﻿open System
+open System.IO
+open System.Threading.Tasks
+
+open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+
+open Giraffe
+
+
+open Shared
+
+let clientPath = Path.Combine("..","Client") |> Path.GetFullPath
+let port = 8085us
+
+let getInitCounter () : Task<Counter> = task { return 42 }
+
+let webApp : HttpHandler =
+  route "/api/init" >=>
+    fun next ctx ->
+      task {
+        let! counter = getInitCounter()
+        return! Successful.OK counter next ctx
+      }
+
+let configureApp  (app : IApplicationBuilder) =
+  app.UseStaticFiles()
+     .UseGiraffe webApp
+
+let configureServices (services : IServiceCollection) =
+    services.AddGiraffe() |> ignore
+
+WebHost
+  .CreateDefaultBuilder()
+  .UseWebRoot(clientPath)
+  .UseContentRoot(clientPath)
+  .Configure(Action<IApplicationBuilder> configureApp)
+  .ConfigureServices(configureServices)
+  .UseUrls("http://0.0.0.0:" + port.ToString() + "/")
+  .Build()
+  .Run()
